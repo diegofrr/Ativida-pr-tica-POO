8ba5dc496785bc8a7e8067c96fda17d1567bd970
Diêgo <Digo@DESKTOP-D40HN2E>
2020-10-19 10:07:04 -0300

    att

--- Atividade2/src/application/Sistema.java
+++ Atividade2/src/application/Sistema.java
@@ -1,6 +1,7 @@
 package application;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+import classes.Pessoa;
 import classes.Empregado;
 import classes.Fornecedor;
 import classes.Administrador;
@@ -13,6 +14,46 @@ public class Sistema {
 	private ArrayList<Administrador> listaAdministradores = new ArrayList();
 	private ArrayList<Vendedor> listaVendedores = new ArrayList();
 	
+	
+	public Pessoa novaPessoa() {
+		String nome = JOptionPane.showInputDialog("NOME");
+		String endereco = JOptionPane.showInputDialog("ENDEREÇO");
+		String telefone = JOptionPane.showInputDialog("TELEFONE");
+		Pessoa _pessoa = new Pessoa(nome, endereco, telefone);
+		return _pessoa;
+	}
+	
+	public void novoEmpregado() {
+		Pessoa _pessoa = novaPessoa();
+		double salario = Double.parseDouble(JOptionPane.showInputDialog("SALÁRIO"));
+		Empregado _empregado = new Empregado(_pessoa.getNome(), _pessoa.getEndereco(), _pessoa.getTelefone(), salario);
+		listaEmpregados.add(_empregado);
+	}
+	
+	public void novoFornecedor() {
+		Pessoa _pessoa = novaPessoa();
+		double credito = Double.parseDouble(JOptionPane.showInputDialog("VALOR CRÉDITO"));
+		double divida = Double.parseDouble(JOptionPane.showInputDialog("VALOR DÍVIDA"));
+		Fornecedor _fornecedor = new Fornecedor(_pessoa.getNome(), _pessoa.getEndereco(), _pessoa.getTelefone(), credito, divida);
+		listaFornecedores.add(_fornecedor);	
+	}
+	
+	public void novoVendedor() {
+		Pessoa _pessoa = novaPessoa();
+		double salario = Double.parseDouble(JOptionPane.showInputDialog("SALÁRIO"));
+		double vendas = Double.parseDouble(JOptionPane.showInputDialog("VALOR VENDAS"));
+		double comissao = Double.parseDouble(JOptionPane.showInputDialog("VALOR COMISSÃO"));
+		Vendedor _vendedor = new Vendedor(_pessoa.getNome(), _pessoa.getEndereco(), _pessoa.getTelefone(), salario, vendas, comissao);
+		listaVendedores.add(_vendedor);
+	}
+	
+	public void novoAdministrador() {
+		Pessoa _pessoa = novaPessoa();
+		double salario = Double.parseDouble(JOptionPane.showInputDialog("SALÁRIO"));
+		double ajudaDeCusto = Double.parseDouble(JOptionPane.showInputDialog("AJUDA DE CUSTO"));
+		Administrador _administrador = new Administrador(_pessoa.getNome(), _pessoa.getEndereco(), _pessoa.getTelefone(), salario, ajudaDeCusto);
+		listaAdministradores.add(_administrador);
+	}
 	
 	public String listarEmpregados() {
 		String listaStr = "";
@@ -44,46 +85,6 @@ public class Sistema {
 			listaStr += _vendedor.toString() + "\n\n";
 		}
 		return listaStr;
-	}
-	
-	public void novoEmpregado() {
-		String nome = JOptionPane.showInputDialog("NOME");
-		String endereco = JOptionPane.showInputDialog("ENDEREÇO");
-		String telefone = JOptionPane.showInputDialog("TELEFONE");
-		double salario = Double.parseDouble(JOptionPane.showInputDialog("SALÁRIO"));
-		Empregado _empregado = new Empregado(nome, telefone, endereco, salario);
-		listaEmpregados.add(_empregado);
-	}
-	
-	public void novoFornecedor() {
-		String nome = JOptionPane.showInputDialog("NOME");
-		String endereco = JOptionPane.showInputDialog("ENDEREÇO");
-		String telefone = JOptionPane.showInputDialog("TELEFONE");
-		double credito = Double.parseDouble(JOptionPane.showInputDialog("VALOR CRÉDITO"));
-		double divida = Double.parseDouble(JOptionPane.showInputDialog("VALOR DÍVIDA"));
-		Fornecedor _fornecedor = new Fornecedor(nome, telefone, endereco, credito, divida);
-		listaFornecedores.add(_fornecedor);	
-	}
-	
-	public void novoVendedor() {
-		String nome = JOptionPane.showInputDialog("NOME");
-		String endereco = JOptionPane.showInputDialog("ENDEREÇO");
-		String telefone = JOptionPane.showInputDialog("TELEFONE");
-		double salario = Double.parseDouble(JOptionPane.showInputDialog("SALÁRIO"));
-		double vendas = Double.parseDouble(JOptionPane.showInputDialog("VALOR VENDAS"));
-		double comissao = Double.parseDouble(JOptionPane.showInputDialog("VALOR COMISSÃO"));
-		Vendedor _vendedor = new Vendedor(nome, telefone, endereco, salario, vendas, comissao);
-		listaVendedores.add(_vendedor);
-	}
-	
-	public void novoAdministrador() {
-		String nome = JOptionPane.showInputDialog("NOME");
-		String endereco = JOptionPane.showInputDialog("ENDEREÇO");
-		String telefone = JOptionPane.showInputDialog("TELEFONE");
-		double salario = Double.parseDouble(JOptionPane.showInputDialog("SALÁRIO"));
-		double ajudaDeCusto = Double.parseDouble(JOptionPane.showInputDialog("AJUDA DE CUSTO"));
-		Administrador _administrador = new Administrador(nome, endereco, telefone, salario, ajudaDeCusto);
-		listaAdministradores.add(_administrador);
 	}
 
 }
