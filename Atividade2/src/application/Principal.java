5ca31d6708b80e95326a5a937122a03a3cf8ebae
Diêgo <Digo@DESKTOP-D40HN2E>
2020-10-22 16:31:51 -0300

    att.

--- Atividade2/src/application/Principal.java
+++ Atividade2/src/application/Principal.java
@@ -2,7 +2,7 @@ package application;
 
 import javax.swing.JOptionPane;
 
-public class Interface {
+public class Principal {
 
 	public static void main(String[] args) {
 		Sistema sistema = new Sistema();
